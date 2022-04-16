0dadaf26ab62ff3bfcb14973c246ad5e631b34f7
Jurreaj92 <jurreaj92@gmail.com>
2022-04-16 18:08:51 -0500

    se actualiza chekpoint

--- Digital House/checkPoint/app.js
+++ Digital House/checkPoint/app.js
@@ -3,10 +3,16 @@ const autos = require ("./autos.js")
 const concesionaria = {
    autos: autos,
  
-   buscarAuto: autos.filter(function(patente){
-      return autos
-
-   })
+   buscarAuto:   
+   // autos.filter(function(patente){
+   //    return autos.patente === patente
+   function filtrarPorPatente(patente){
+    const autosFiltrados = autos.filter(function(autos){
+        return autos.patente == patente;
+    });
+    return autosFiltrados;
+   }
 }
 
-concesionaria.buscarAuto('JJK116')
+console.log(autosFiltrados)
+console.log(autos)
